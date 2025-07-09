0121c827d481159be8e73fa6bb79b8e2d790480a
JoaquinJS18 <jhinfire@gmail.com>
2025-07-08 22:03:59 -0300

    Vista producto modificada.

--- src/app/components/product-detail/product-detail.ts
+++ src/app/components/product-detail/product-detail.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { Producto, ProductService } from '@app/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [MatCardModule,MatButtonModule],
+  imports: [MatCardModule,MatButtonModule,CommonModule,MatIconModule],
   templateUrl: './product-detail.html',
+  styleUrls: ['./product-detail.css'],
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit {
+  producto!: Producto;
+  loading = false;
+  error = '';
+  imagenes: string[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.productService.getProductById(id).subscribe({
+        next: res => {
+          this.producto = res.data;
+          this.loading = false;
+          this.producto = res.data;
+          this.imagenes = [
+            this.producto.imagen_url, // imagen principal
+          ];
+        },
+        error: (err) => { 
+          this.error = 'No se pudo cargar el producto.';
+          this.loading = false;
+        }
+      });
+    } else {
+      this.error = 'ID de producto inválido.';
+      this.loading = false;
+    }
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+
+export interface Producto {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  stock: number;
+  imagen_url: string;
+  mercadoPagoLink: string;
+  categoria?: { id: number, nombre: string };
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  http = inject(HttpClient);
   apiUrl = environment.API_URL;
+  apiUrlLocal = environment.API_LOCAL + "/productos";
+
+  constructor(private http: HttpClient) { }
 
   getProducts() {
     return this.http.get(`${this.apiUrl}/products`);
+  }
+
+  getProductById(id: number): Observable<{ success: boolean; data: Producto; message: string }> {
+    return this.http.get<{ success: boolean; data: Producto; message: string }>(`${this.apiUrlLocal}/${id}`);
   }
 }
